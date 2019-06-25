07febc4080f1bf79710f30b5ea3b0421d030b3fb
PujaKumari30 <37130465+PujaKumari30@users.noreply.github.com>
2019-06-25 21:46:34 +0530

    Update BFS.java

--- TreePrograms/src/com/tree/prog/BFS.java
+++ TreePrograms/src/com/tree/prog/BFS.java
@@ -44,6 +44,22 @@ public class BFS
 		}
 	}
 	
+	/* Using Queue */
+	static void levelOrder(Node node) 
+ 	{
+        	Queue<Node> queue = new LinkedList<Node>();
+        	queue.add(node);
+        	while(!queue.isEmpty())
+        	{
+            		Node temp = queue.poll();
+           		System.out.print(temp.data+" ");
+            		if(temp.left!=null)
+            		queue.add(temp.left);
+            		if(temp.right!=null)
+            		queue.add(temp.right);
+        	}
+    	}
+	
 	public static void main(String args[])
 	{
 		BFS bfs = new BFS();
