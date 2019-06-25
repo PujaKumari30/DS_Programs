8114326b0a17b9eb044cbf8dbec4129ade4dd6b7
PujaKumari30 <37130465+PujaKumari30@users.noreply.github.com>
2019-06-25 21:16:20 +0530

    Update ReverseListInPairs.java

--- LinkedListProg/src/com/linkedlist/programs/ReverseListInPairs.java
+++ LinkedListProg/src/com/linkedlist/programs/ReverseListInPairs.java
@@ -4,6 +4,8 @@
 package com.linkedlist.programs;
 
 public class ReverseListInPairs {
+	
+	//Method -1 Recursion
 	public Node ReverseLinkedListInPairs(Node head)
 	{
 		Node temp;
@@ -16,5 +18,44 @@ public class ReverseListInPairs {
 		return ReverseLinkedListInPairs(head.next);
 		
 	}
+	
+	//Method -2 swapping only data
+	public static void pairwiseSwap(Node node)
+    {
+        int temp;
+        Node current = node;
+       while(current!=null && current.next!=null)
+       {
+           temp = current.data;
+           current.data = current.next.data;
+           current.next.data = temp;
+           current = current.next.next;
+       }
+    }
+	
+	//Method -3 Iterative
+	public static void pairwiseSwap(Node node)
+    {
+        Node current = node,prev = null,prev1=null,next=null,next1=null;
+       	while(current!=null && current.next!=null)
+       	{
+       	    prev = current;
+       	    next = current.next;
+       	    next1 = current.next.next;
+       	    next.next = prev;
+       	    if(prev1!=null)
+       	    {
+       	        prev1.next=next;
+       	        prev1 = prev;
+       	    }
+       	    else
+       	    {
+       	        node = next;
+       	        prev1= prev;
+       	    }
+       	    
+       	    current = next1;
+       	}
+    }
 
 }
